d736937a591a1add492a7b4136ddf7d533d5adf3
Brant Fuller <brant.fuller@gmail.com>
2021-04-20 14:29:12 -0500

    Update FoodTruck#check_stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require './lib/food_truck'
+require './lib/item'
 
 RSpec.describe FoodTruck do
   describe '#initialize' do
@@ -15,6 +16,7 @@ RSpec.describe FoodTruck do
   end
   describe 'check_stock' do
     food_truck = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     it 'returns 0 if an item is has not been stocked' do
       expect(food_truck.check_stock(item1)).to eq(0)
     end
