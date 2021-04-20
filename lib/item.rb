94830fd1d66f96bf20b7f8ffa9732435b34a1794
Brant Fuller <brant.fuller@gmail.com>
2021-04-20 14:13:24 -0500

    Create Item instance variables and attr_reader

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,8 @@
 class Item
-  def initialize(item_information)    
+  attr_reader :name,
+              :price
+  def initialize(item_information)
+    @name = item_information[:name]
+    @price = item_information[:price]
   end
 end
