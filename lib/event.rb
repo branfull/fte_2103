4e5381fab89af60f4f966022850784b932b28cb4
Brant Fuller <brant.fuller@gmail.com>
2021-04-20 15:05:06 -0500

    Create Event#food_trucks_that_sell method

--- lib/event.rb
+++ lib/event.rb
@@ -15,4 +15,17 @@ class Event
       food_truck.name
     end
   end
+
+  # refactor if time allows
+  def food_trucks_that_sell(item_in_question)
+    array = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.keys.each do |item|
+        if item == item_in_question
+          array.push(food_truck)
+        end
+      end
+    end
+    array
+  end
 end
