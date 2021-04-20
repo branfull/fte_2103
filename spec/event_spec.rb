fc9dcc920c42f3887d4ef6807d21fdb05e8417d8
Brant Fuller <brant.fuller@gmail.com>
2021-04-20 14:46:17 -0500

    Create Event#initialize tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -2,9 +2,15 @@ require './lib/event'
 
 RSpec.describe Event do
   describe '#initialize' do
-    event = Event.new("South Pearl Street Farmers Market")
+    event = Event.new('South Pearl Street Farmers Market')
     it 'exists' do
       expect(event).to be_an_instance_of(Event)
+    end
+    it 'has a name' do
+      expect(event.name).to eq('South Pearl Street Farmers Market')
+    end
+    it 'has food trucks' do
+      expect(event.food_trucks).to eq([])
     end
   end
 end
