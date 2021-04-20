d23d465e1a0ba2494a1dcc4402c5b006944fc25d
Brant Fuller <brant.fuller@gmail.com>
2021-04-20 14:31:56 -0500

    Create FoodTruck#stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -20,5 +20,17 @@ RSpec.describe FoodTruck do
     it 'returns 0 if an item is has not been stocked' do
       expect(food_truck.check_stock(item1)).to eq(0)
     end
+    it 'returns the quantity of an item in the inventory' do
+      food_truck.stock(item1, 30)
+      expect(food_truck.check_stock(item1)).to eq(30)
+    end
+  end
+  describe 'stock' do
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    it 'adds an item and quantity to the inventory hash' do
+      expect(food_truck.inventory).to eq(item1: 30)
+    end
   end
 end
