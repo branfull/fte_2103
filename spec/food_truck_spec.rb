191564bbb26918a9a5ae5b7e3a9cf4261bdade46
Brant Fuller <brant.fuller@gmail.com>
2021-04-20 14:18:23 -0500

    Create FoodTruck#initialize tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,9 +2,15 @@ require './lib/food_truck'
 
 RSpec.describe FoodTruck do
   describe '#initialize' do
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
     it 'exists' do
       expect(food_truck).to be_an_instance_of(FoodTruck)
+    end
+    it 'has a name' do
+      expect(food_truck.name).to eq('Rocky Mountain Pies')
+    end
+    it 'has an inventory' do
+      expect(food_truck.inventory).to eq({})
     end
   end
 end
