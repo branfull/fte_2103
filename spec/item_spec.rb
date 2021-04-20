3b9e0e1c08c5f30ed95551be733945b8adffa521
Brant Fuller <brant.fuller@gmail.com>
2021-04-20 14:11:48 -0500

    Create Item#initialize tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,8 +3,15 @@ require './lib/item'
 RSpec.describe Item do
   describe '#initialize' do
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     it 'exists' do
-      expect(item1).to be_an_instance_of(Item)
+      expect(item2).to be_an_instance_of(Item)
+    end
+    it 'has a name' do
+      expect(item2.name).to eq('Apple Pie (Slice)')
+    end
+    it 'has a price' do
+      expect(item2.price).to eq('$2.50')
     end
   end
 end
